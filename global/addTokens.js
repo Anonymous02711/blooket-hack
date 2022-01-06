1f7c3d441acd2bf945ed2e26f8369596a8a2c72c
Anonymous02711 <97136919+Anonymous02711@users.noreply.github.com>
2022-01-06 08:56:39 +0700

    Update addTokens.js
    If u accept this, I will share this hack blooket and more people know to hack and know u.

--- global/addTokens.js
+++ global/addTokens.js
@@ -13,13 +13,13 @@ async function getName() {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (1000 daily)'));
 
-    if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.');
+    if (tokens > 1000) {
+        alert('You can only add up to 1000 tokens daily.');
     };
 
-    const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
+    const response = await fetch('https://api.blooket.com/api/Dol_cosmos2/add-rewards', {
         method: "PUT",
         headers: {
             "referer": "https://www.blooket.com/",
@@ -29,7 +29,7 @@ async function addCurrencies() {
         body: JSON.stringify({
             addedTokens: tokens,
             addedXp: 300,
-            name: await getName()
+            name: await Dol_cosmos2()
         })
     });
 
